ec472c286e5a120a81a81d08df94178a26e2b511
Omar Elnemr <omar.elnemr@nagwa.com>
2025-07-21 16:16:32 +0300

    Update SHA256 checksum for CheckBooks formula

--- Formula/check_books.rb
+++ Formula/check_books.rb
@@ -2,7 +2,7 @@ class CheckBooks < Formula
   desc "checkBooks — DCli-powered CLI for checking your books xml files"
   homepage "https://github.com/nemr0/homebrew-check_books"
   url "https://raw.githubusercontent.com/nemr0/homebrew-check_books/main/binaries/checkbooks-0.1.2-macos.tar.gz"
-  sha256 "e6ebcc4055828e4ecdb5b9aa0a4e693a85c5ab0907b7ec2cab7930746f2890ff"
+  sha256 "378e0e9cd72f0307c3423e436a5e9f502e59ee3738f17d58081002d6fa3c860c"
   license "MIT"
 
   def install
