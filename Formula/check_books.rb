bd05e4d71d1a9ca77b6ebf7f4927e5d3d5579c59
Omar Elnemr <omar.elnemr@nagwa.com>
2025-07-21 15:23:49 +0300

    Add initial implementation of BrewCheckBooks formula and example log file

---
+++ Formula/check_books.rb
@@ -0,0 +1,17 @@
+class BrewCheckBooks < Formula
+  desc "checkBooks — DCli-powered CLI for checking your books xml files"
+  homepage "https://github.com/nemr0/homebrew-check_books"
+  url "https://raw.githubusercontent.com/nemr0/homebrew-check_books/main/binaries/check_books-0.1.0-macos.tar.gz"
+  sha256 "e6ebcc4055828e4ecdb5b9aa0a4e693a85c5ab0907b7ec2cab7930746f2890ff"
+  license "MIT"
+
+  def install
+    # Rename the binary to `checkBooks` on install
+    bin.install "check_books" => "checkBooks"
+  end
+
+  test do
+    # A simple smoke test to verify the binary runs
+    assert_match "Usage", shell_output("#{bin}/checkBooks --help")
+  end
+end
---
+++ readme.md
@@ -0,0 +1,30 @@
+# check_books
+
+A Dart command-line application to manage and validate Hindawi Foundation book xml file.
+
+## Features
+
+- Scan a directory of book files and verify metadata consistency  
+- List all books and their details and log to json file check `/example/log.json`
+
+## Installation
+
+### Via Homebrew
+
+```bash
+brew tap nemr0/check_books https://github.com/nemr0/homebrew-check_books
+brew install check_books
+```
+
+## Usage
+
+Once installed, use the `checkBooks` executable:
+
+```bash
+checkBooks 
+```
+
+
+## License
+
+MIT © [Omar Elnemr](https://github.com/nemr0)
