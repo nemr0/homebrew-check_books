c8cfa09fac9846ffbe801fc49a8c2c8af4fdf484
Omar Elnemr <omar.elnemr@nagwa.com>
2025-07-21 16:13:33 +0300

    added version 0.1.2

--- Formula/check_books.rb
+++ Formula/check_books.rb
@@ -1,7 +1,7 @@
 class CheckBooks < Formula
   desc "checkBooks — DCli-powered CLI for checking your books xml files"
   homepage "https://github.com/nemr0/homebrew-check_books"
-  url "https://raw.githubusercontent.com/nemr0/homebrew-check_books/main/binaries/checkbooks-0.1.1-macos.tar.gz"
+  url "https://raw.githubusercontent.com/nemr0/homebrew-check_books/main/binaries/checkbooks-0.1.2-macos.tar.gz"
   sha256 "e6ebcc4055828e4ecdb5b9aa0a4e693a85c5ab0907b7ec2cab7930746f2890ff"
   license "MIT"
 
