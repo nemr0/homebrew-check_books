360ec718b0c2578fc1603a0dbd37033353630a25
Omar Elnemr <omar.elnemr@nagwa.com>
2025-07-22 10:05:13 +0300

    Added CheckBooks version 0.1.3 and updated .gitignore

--- Formula/check_books.rb
+++ Formula/check_books.rb
@@ -1,8 +1,8 @@
 class CheckBooks < Formula
   desc "checkBooks — DCli-powered CLI for checking your books xml files"
   homepage "https://github.com/nemr0/homebrew-check_books"
-  url "https://raw.githubusercontent.com/nemr0/homebrew-check_books/main/binaries/checkbooks-0.1.2-macos.tar.gz"
-  sha256 "378e0e9cd72f0307c3423e436a5e9f502e59ee3738f17d58081002d6fa3c860c"
+  url "https://raw.githubusercontent.com/nemr0/homebrew-check_books/main/binaries/checkbooks-0.1.3-macos.tar.gz"
+  sha256 "a32426d0f25f492886fede9ffc85adea7984a9a4c7d25dfe82e3d2aadc38f779"
   license "MIT"
 
   def install
